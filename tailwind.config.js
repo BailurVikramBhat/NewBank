159e2d8b14158432f19a37ab0cc3db2d7597fe27
Vikram Bhat <bailuvikrambhat@gmail.com>
2024-04-12 01:04:47 +0530

    top nav bar changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    fontFamily: {
+      mont: ["Montserrat", "sans-serif"],
+      inter: ["Inter", "serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
