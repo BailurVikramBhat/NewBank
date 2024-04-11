59e9a06702d1a147064a49b9a81adfe61127c01a
Vikram Bhat <bailuvikrambhat@gmail.com>
2024-04-12 01:04:47 +0530

    top nav bar changes

---
+++ src/app/topnav/topnav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topnav',
+  standalone: true,
+  imports: [],
+  templateUrl: './topnav.component.html',
+  styleUrl: './topnav.component.css'
+})
+export class TopnavComponent {
+
+}
